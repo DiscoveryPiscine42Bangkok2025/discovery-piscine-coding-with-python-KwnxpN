c0737fa42df43553957dfebf7f58487f1461c01f
KwnxpN <tonnum.kawin@gmail.com>
2025-09-16 15:36:34 +0700

    chore: comment's typo

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -4,7 +4,7 @@ def main():
     """This function is the main function."""
 
     num = int(input("Enter a number less than 25 : "))
-    if num > 25: return print("Error") # Error casse more than 25
+    if num > 25: return print("Error") # Error case more than 25
 
     while num <= 25:
         print(f"inside the loop, my variable is {num}")
